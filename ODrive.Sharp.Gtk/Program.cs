0d514c3681aae3b38a6e88d7f725255943fd0711
Bruno B. de Souza <knuxbbs@gmail.com>
2019-08-08 21:40:44 -0300

    Initial commit

---
+++ ODrive.Sharp.Gtk/Program.cs
@@ -0,0 +1,24 @@
+using System;
+using Gtk;
+
+namespace ODrive.Sharp.Gtk
+{
+    class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            Application.Init();
+
+            // ReSharper disable once StringLiteralTypo
+            var app = new Application("org.sharp.odrive", GLib.ApplicationFlags.None);
+            app.Register(GLib.Cancellable.Current);
+
+            var win = new MainWindow();
+            app.AddWindow(win);
+
+            win.Show();
+            Application.Run();
+        }
+    }
+}
