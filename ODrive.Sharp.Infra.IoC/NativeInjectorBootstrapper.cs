8071c68a5da041f7affefe6c7d552870f5cfb1b4
Bruno B. de Souza <knuxbbs@gmail.com>
2019-11-19 23:36:32 -0300

    Upgrade to dotnet 3 and adds dependency injection.

---
+++ ODrive.Sharp.Infra.IoC/NativeInjectorBootstrapper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using ODrive.Sharp.Application.Interfaces;
+using ODrive.Sharp.Application.Services;
+
+namespace ODrive.Sharp.Infra.IoC
+{
+    public class NativeInjectorBootstrapper
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            services.AddScoped<IGoogleDriveAppService, GoogleDriveAppService>();
+        }
+    }
+}
